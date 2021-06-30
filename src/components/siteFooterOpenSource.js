8033cc59710575ea7e3c5576be55ff50039ee888
fourzans <fourzans@gmail.com>
2021-06-29 21:48:56 -0500

    New Footer and Improved Layout - Adding new footer - Updating 404 page - Updating layout to include footers - Updating site metadata - Adding styles, improving responsiveness

---
+++ src/components/siteFooterOpenSource.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+    return (
+        <footer className="opensource">
+            <p>&copy; marianofourzans &bull; {new Date().getFullYear()} &bull; all code <a href="http://opensource.org/licenses/MIT" rel="nofollow">MIT license</a></p>
+        </footer>
+    )
+}
